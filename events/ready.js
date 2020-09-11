d44fe83d8b00be888afaf5ae5417c55bf0dc3da8
Takasakiii <lucasmc2709@live.com>
2020-09-11 19:15:24 -0300

    Bot status

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,14 @@
-const { client } = require("../index");
+const { client, dbBotList } = require("../index");
 
 
 client.on("ready", () => {
+    setStatus();
+    setInterval(setStatus, 300000);
     console.log("Bot ligou!");
 })
+
+
+async function setStatus(){
+    const botsCount = await dbBotList.Bots.countDocuments().exec();
+    client.user.setActivity("todos os " + botsCount + " bots que estão na botlist 😋", {type: "WATCHING"})
+}
