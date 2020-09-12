6aced963892faf7c022bb24ac5d66dc524db4092
Takasakiii <lucasmc2709@live.com>
2020-09-12 02:35:58 -0300

    tentando desbugar o motivo no modlog

--- utils/modlog.js
+++ utils/modlog.js
@@ -31,7 +31,7 @@ module.exports = async (guild) => {
         if(type){
             const channel = guild.channels.cache.get(config.bot.guilds.main.channels.modlog) 
             channel.send(new MessageEmbed()
-                .setDescription("Motivo: `" + audict.reason ? audict.reason : "Sem motivo informado." + "`")
+                .setDescription(`Motivo: \`${ audict.reason ? audict.reason : "Sem motivo informado."}\``)
                 .setTitle(`${audict.executor.tag} ${type} ${audict.target.tag} (${audict.target.id})`)
                 .setColor(config.bot.primaryColor)
             );
