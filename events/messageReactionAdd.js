2c55a40323726c339d28c232dac20120847c4adb
Takasakiii <lucasmc2709@live.com>
2020-09-20 01:08:56 -0300

    teste 6000

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -6,7 +6,7 @@ client.on("messageReactionAdd", (msgreaction, user) => {
         return;
     
     console.log(msgreaction.emoji.name);
-    if(msgreaction.emoji.name == "⭐" && msgreaction.count >= 1 && !msgreaction.message.reactions.cache.has("🌟")){
+    if(msgreaction.emoji.name == "⭐" && msgreaction.count >= 1 && !msgreaction.message.reactions.cache.has("star2")){
         const channel = msgreaction.message.guild.channels.cache.get(config.bot.guilds.main.channels.starboard);
         console.log("entrou");
         const starmsg = new MessageEmbed()
