d8acc5e8a3156d1da7f0c5098fb7bd55270363c3
Takasakiii <lucasmc2709@live.com>
2020-10-18 13:57:31 -0300

    correção de type

--- src/events/guild/message/edited.ts
+++ src/events/guild/message/edited.ts
@@ -1,5 +1,6 @@
 import zuraaa from '../../../'
+import { Message } from 'discord.js'
 
 zuraaa.client.on('messageUpdate', (old, newer) => {
-    zuraaa.client.emit('message', newer)
+    zuraaa.client.emit('message', newer as Message)
 })
