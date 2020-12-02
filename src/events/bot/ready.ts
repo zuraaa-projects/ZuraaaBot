85581623ac7006ec2090b6d61a9ee000f3219505
Yummiii <Leeo05050@outlook.com>
2020-12-02 15:14:15 -0300

    eu so um merda parte 3000

--- src/events/bot/ready.ts
+++ src/events/bot/ready.ts
@@ -14,22 +14,4 @@ function setStatus(){
     api.getBotCount().then(count => {
         zuraaa.client.user?.setActivity("Todos os " + count.bots_count + " bots que estão na botlist 😋", {type: "WATCHING"})
     }).catch(console.warn)
-
-    api.getTopBots().then(async bots => {
-        const main = config.bot.guilds.main;
-        const servidor = zuraaa.client.guilds.cache.get(main.id);
-
-        (await servidor?.roles.fetch(main.otherroles.topbots))?.members.map(m => {
-            m.roles.remove(main.otherroles.topbots);
-        });
-
-        (await servidor?.roles.fetch(main.otherroles.topbotdevs))?.members.map(m => {
-            m.roles.remove(main.otherroles.topbotdevs);
-        });
-
-        for(const bot of bots){
-            servidor?.member(bot._id)?.roles.add(main.otherroles.topbots);
-            servidor?.member(bot.owner)?.roles.add(main.otherroles.topbotdevs);
-        }
-    }).catch(console.warn)
 }
