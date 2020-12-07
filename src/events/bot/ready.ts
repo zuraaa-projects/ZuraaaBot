d87028f846353cb138ba9ed629fac8da936bb883
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-12-07 07:11:34 -0300

    eu tentei Shiba 😔

--- src/events/bot/ready.ts
+++ src/events/bot/ready.ts
@@ -1,14 +1,14 @@
 import zuraaa from '../../'
 import ZuraaaApi from '../../modules/api/zuraaaapi'
 import config from '../../../config.json'
-import { DiscordAPIError, Guild, Role } from 'discord.js'
+import { Guild, Role } from 'discord.js'
 
 const api = new ZuraaaApi()
 
 zuraaa.client.on('ready', () => {
     setStatus()
     setInterval(setStatus, 300000)
-    // updateTop()
+    // updateTop() //! INDEV
     console.log(zuraaa.client.user?.username + ' se encontra online!')
 })
 
@@ -20,26 +20,31 @@ function setStatus() {
 
 async function updateTop() {
     const topBots = await api.getTopBots()
-    const guildConfig = config.bot.guilds.main
-    const guild = zuraaa.client.guilds.cache.get(guildConfig.id)
+    const mainGuild = config.bot.guilds.main
+    const zuraaaDiscord = zuraaa.client.guilds.cache.get(mainGuild.id)!
 
-    const topDevsRole = await guild?.roles.fetch(guildConfig.otherroles.topbotdevs)
-    const topOwners = topBots.map(bot => bot.owner)
-    updateMembersRole(topDevsRole!, topOwners, guild!)
+    const topDevsRole = await zuraaaDiscord?.roles.fetch(mainGuild.otherroles.topbotdevs)
+    const topDevsIds = topBots.map(bot => bot.owner)
+    updateRoles(topDevsRole!, topDevsIds, zuraaaDiscord)
 
-    const topBotsRole = await guild?.roles.fetch(guildConfig.otherroles.topbots)
+    const topBotsRole = await zuraaaDiscord?.roles.fetch(mainGuild.otherroles.topbots)
     const topBotsIds = topBots.map(bot => bot._id)
-    updateMembersRole(topBotsRole!, topBotsIds, guild!)
+    updateRoles(topBotsRole!, topBotsIds, zuraaaDiscord)
 
     setTimeout(updateTop, 30000)
 }
 
-async function updateMembersRole(role: Role, newIds: string[], guild: Guild) {
-    const added: string[] = []
+async function updateRoles(role: Role, newIds: string[], guild: Guild) {
+    // TODO: REFATORAR TODO COMANDO DE ATUALIZAR OS CARGOS
+    // ! DEVEMOS PENSAR EM UMA FORMA MELHOR DE FAZER
+    // ? Como devemos fazer?
+    // * Nota: eu passei 3 horas olhando isso, não consigo pensar 
+    // * em como fazer sem usar o cache e sem dar fetch em todos 
+    // * os usuários (impossivel) pois nem o Discord e nem o DiscordJS possuem 
+    // * uma forma de dar fetch em todos os membros **de uma determinada role**
 
-        // TODO: Consertar a remoção de cargos dos membros que não sejam mais topvoted
-
-        newIds.filter(id => !added.includes(id)).forEach(memberId => {
-        guild.members.cache.get(memberId)?.roles.add(role)
-    })
+    // ? Possiveis soluções:
+    //// Dar fetch em todos os usuários e depois comparar (impossível, limite de usuários por fetch)
+    //// Dar fetch apenas nos usuários de uma determinada role (não encontramos como)
+    // * (Estou sem ideias)
 }
