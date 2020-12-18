a1fa55f1d259c1c9e3954010b5f5ab3398324220
LuckShiba <luckshiba@protonmail.com>
2020-12-18 20:40:12 -0300

    ok, isso é pra estar pegando

--- src/modules/utils/bot/modlogs.ts
+++ src/modules/utils/bot/modlogs.ts
@@ -1,33 +1,34 @@
 import config from '../../../../config.json'
 import { Guild, MessageEmbed, TextChannel, User } from 'discord.js'
 
+let lastLogId = ''
+
 export default (guild: Guild): void => {
   guild.fetchAuditLogs({
     limit: 1
   }).then(audictList => {
     const audictEntry = audictList.entries.first()
-    let type = ''
-    switch (audictEntry?.action) {
-      case 'MEMBER_KICK':
-        type = 'expulsou'
-        break
-      case 'MEMBER_BAN_ADD':
-        type = 'baniu'
-        break
-      case 'MEMBER_BAN_REMOVE':
-        type = 'desbaniu'
-        break
+    if (audictEntry === undefined || audictEntry.id === lastLogId) {
+      return
     }
-
-    if (type !== '') {
-      const userTargget = audictEntry?.target as User
+    const types: {
+      [keyof: string]: string | undefined
+    } = {
+      MEMBER_KICK: 'expulsou',
+      MEMBER_BAN_ADD: 'baniu',
+      MEMBER_BAN_REMOVE: 'desbaniu'
+    }
+    const type = types[audictEntry.action]
+    if (type !== undefined) {
+      const userTargget = audictEntry.target as User
       const channel = guild.channels.cache.get(config.bot.guilds.main.channels.modlog) as TextChannel
-      const motivo = audictEntry?.reason ?? 'Sem motivo informado.'
+      const motivo = audictEntry.reason ?? 'Sem motivo informado.'
       channel.send(new MessageEmbed()
         .setDescription(`Motivo: \`${motivo}\``)
-        .setTitle(`${audictEntry?.executor.tag as string} ${type} ${userTargget.tag} (${userTargget.id})`)
+        .setTitle(`${audictEntry.executor.tag} ${type} ${userTargget.tag} (${userTargget.id})`)
         .setColor(config.bot.primaryColor)
       ).catch(console.error)
+      lastLogId = audictEntry.id
     }
   }).catch(console.error)
 }
