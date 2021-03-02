0460c8a802ce14e91577fd9fba1823ca482d3585
KuryKat <57152799+KuryKat@users.noreply.github.com>
2021-03-02 15:27:29 +0000

    z.removebot avisa no site-logs e no DM do owner 😊

--- src/commands/botlist/removebot.ts
+++ src/commands/botlist/removebot.ts
@@ -1,6 +1,7 @@
 import emojis from '@/emojis.json'
+import config from '@/config.json'
 import { BaseCommand, Command, HelpInfo } from '@modules/handler'
-import { MessageEmbed } from 'discord.js'
+import { MessageEmbed, TextChannel } from 'discord.js'
 import ZuraaaApi from '@modules/api/zuraaaapi'
 
 @Command('removebot', 'deletebot', 'rbot', 'dbot')
@@ -10,7 +11,7 @@ import ZuraaaApi from '@modules/api/zuraaaapi'
   usage: ['@bot', '{id}']
 })
 class RemoveBot extends BaseCommand {
-  execute (): void {
+  async execute (): Promise<void> {
     const botId = this.msg.mentions.users.first()?.id ?? this.args[0]
 
     if (botId === undefined) {
@@ -22,18 +23,37 @@ class RemoveBot extends BaseCommand {
     }
 
     const api = new ZuraaaApi()
+    const bot = await api.getBot(botId)
 
-    api.removeBot(botId, this.msg.author.id).then(result => {
+    api.removeBot(botId, this.msg.author.id).then(async result => {
       if (!result.deleted) {
-        return this.msg.channel.send(new MessageEmbed()
+        return await this.msg.channel.send(new MessageEmbed()
           .setColor('RED')
           .setTitle(`${emojis.error.name} | Bot não está cadastrado.`)
         )
       }
+
+      let reason = 'Sem motivo informado.'
+      if (this.args.slice(1).join(' ') !== '') {
+        reason = this.args.slice(1).join(' ')
+      }
+
       this.msg.guild?.member(botId)?.kick()
         .catch(console.error)
       this.msg.react(emojis.ok.id)
         .catch(console.error)
+
+      const embed = new MessageEmbed()
+        .setColor('GREEN')
+        .setTitle('Sucesso!')
+        .setFooter(`Removido por: ${this.msg.author.username}#${this.msg.author.discriminator}`)
+        .setDescription(`O bot \`${bot?.username as string}#${bot?.discriminator as string}\` foi removido da Botlist pelo seguinte motivo: \`${reason}\``)
+
+      const siteLogs = this.msg.guild?.channels.cache.get(config.bot.guilds.main.channels.sitelog) as TextChannel
+      const ownerDM = this.msg.guild?.members.cache.get(bot?.owner as string)
+
+      await siteLogs.send(embed)
+      await ownerDM?.send(embed)
     }).catch(() => {
       this.msg.react(emojis.error.id)
         .catch(console.error)
