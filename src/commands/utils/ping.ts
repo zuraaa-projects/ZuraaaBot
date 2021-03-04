c12a9c5b6a6c641591bad1c5e81c91f1c2733e0d
Hiekki <70537353+EvasiveTechGroup@users.noreply.github.com>
2021-03-04 12:50:53 +0000

    Ping Command : )

---
+++ src/commands/utils/ping.ts
@@ -0,0 +1,15 @@
+import zuraaa from '@bot'
+import { BaseCommand, Command, HelpInfo } from '@modules/handler'
+@Command('latencia', 'ping')
+@HelpInfo({
+  module: 'Utils',
+  description: 'Mostra a latencia do bot',
+  usage: ['z.ping',]
+})
+class Ping extends BaseCommand {
+  execute (): void { 
+this.msg.channel.send(`Ping: ${Date.now() - this.msg.createdTimestamp}ms\nWebSocket:  ${zuraaa.client.ws.ping}ms`)
+  }
+}
+
+export default Ping
