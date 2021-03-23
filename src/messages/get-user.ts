a3be7b72902a3182461900e2e2f8c97e3ef0678d
LuckShiba <LuckShiba@protonmail.com>
2021-03-22 21:31:39 -0300

    tirando logs (vontade de suicidio > 4000)

--- src/messages/get-user.ts
+++ src/messages/get-user.ts
@@ -19,13 +19,10 @@ export function getUser (client: Client, conn: Connection): void {
       console.log('consumed channel')
       if (msg != null) {
         const id = msg.content.toString()
-        console.log('tring to fetch user', id)
         client.users.fetch(id).then(user => {
-          console.log('received', user.username)
           channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(user)), {
             correlationId: msg.properties.correlationId
           })
-          console.log('sent; ACKing')
           channel.ack(msg)
         }).catch(console.error)
       }
