eb7ee161cc4ece44907d8813000439a144e768a6
LuckShiba <LuckShiba@protonmail.com>
2021-03-22 21:26:06 -0300

    adicionando logs pra tentar descobrir o problema

--- src/messages/get-user.ts
+++ src/messages/get-user.ts
@@ -16,14 +16,16 @@ export function getUser (client: Client, conn: Connection): void {
     channel.prefetch(1)
 
     channel.consume(queue, (msg) => {
+      console.log('consumed channel')
       if (msg != null) {
         const id = msg.content.toString()
-
+        console.log('tring to fetch user', id)
         client.users.fetch(id).then(user => {
+          console.log('received', user.username)
           channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(user)), {
             correlationId: msg.properties.correlationId
           })
-
+          console.log('sent; ACKing')
           channel.ack(msg)
         }).catch(console.error)
       }
